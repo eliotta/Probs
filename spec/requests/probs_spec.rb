41209697e440a6afc0b55e0d83ad759e6e08e0d0
Eric Liotta <eliotta@outlook.com>
2014-04-03 19:54:52 -0400

    second

--- spec/requests/probs_spec.rb
+++ spec/requests/probs_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Probs" do
-  describe "GET /probs" do
+  describe "GET /probs  " do
     it 'display some probs' do
       visit probs_path
       page.should have_content 'some content'
